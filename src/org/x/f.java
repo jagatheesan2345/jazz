13622e4bc3843ddf1259fa2f05ae6ff88c4832b0
jagatheesan2345 <theesun6195@gmail.com>
2023-01-12 18:44:18 +0530

    hii

--- src/org/x/f.java
+++ src/org/x/f.java
@@ -2,10 +2,23 @@ package org.x;
 
 public class f {
 
+	public void java(int age) 
+	{
+		System.out.println("age of the person : "+age);
+
+	}
+	public void selenium(String name) 
+	{
+		System.out.println("age of the person : "+name);
+
+	}
 	public static void main(String[] args) {
 		int a=10,b=20;
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(a+b);
+		f fi= new f();
+		fi.java(27);
+		fi.selenium("jazz");
 	}
 }
